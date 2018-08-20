4f97144eb06d2066bea149289cdeb5b0fb380b52
Josh Hepworth <jrhepworth12@gmail.com>
2018-08-20 16:32:42 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,17 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cards.push(cardsInPlay);
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cards.push(cardsInPlay);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2 || cardsInPlay[0] === cardsInPlay[2]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
